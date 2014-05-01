9f88d4d7bdfe56d7b447f7f0959a244c11a1b23a
Pedro Hernandez <pih24@cs.drexel.edu>
2014-05-01 14:07:12 -0400

    worked

--- TypeForYourLife/src/MainFrame.java
+++ TypeForYourLife/src/MainFrame.java
@@ -1,4 +1,20 @@
+import java.awt.BorderLayout;
 
-public class MainFrame {
+import javax.swing.JFrame;
 
+
+public class MainFrame extends JFrame{
+	
+	private MenuPanel menupanel;
+	
+	public void MainFrame(){
+		
+	
+	
+	
+	
+	 add(menupanel, BorderLayout.CENTER);
+	
+	
+	}
 }
---
+++ TypeForYourLife/src/MenuPanel.java
@@ -0,0 +1,15 @@
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+
+public class MenuPanel extends JPanel{
+	
+	private JButton startButton;
+	private JButton leaderboardsButton;
+	private JButton creditsButton;
+	
+	public void MenuPanel(){
+		
+	}
+
+}
